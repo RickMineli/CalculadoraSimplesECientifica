772ff901e244af9e8b59042c3f491f93bcfbdf1b
Ricardo Mineli <rick_mineli@hotmail.com>
2018-12-26 21:47:48 -0200

    Adicionado Calculadora Cientifica

--- src/main/java/com/github/rickmineli/calculadoraapi/model/TipoDeCalculo.java
+++ src/main/java/com/github/rickmineli/calculadoraapi/model/TipoDeCalculo.java
@@ -22,9 +22,35 @@ public enum TipoDeCalculo {
     CIENTIFICA {
         @Override
         public Double executaEquacao(List<Object> equacaoEmLista) {
-            return null;
+            Double primeroNumero;
+            Double segundoNumero;
+            Operacao operacao;
+
+            for (int i = 1; i < equacaoEmLista.size(); i = i + 2) {
+                System.out.println("Entrou no for");
+                if (Operacao.MULTIPLICACAO == Operacao.valueOf((String) equacaoEmLista.get(i))) {
+                    System.out.println("Entrou no if multi");
+                    equacaoEmLista = fazMultiEDiviCientifica(Operacao.MULTIPLICACAO, equacaoEmLista, i);
+                }
+                if (Operacao.DIVISAO == Operacao.valueOf((String) equacaoEmLista.get(i))) {
+                    equacaoEmLista = fazMultiEDiviCientifica(Operacao.DIVISAO, equacaoEmLista, i);
+                }
+            }
+
+            System.out.println(equacaoEmLista);
+            return TipoDeCalculo.SIMPLES.executaEquacao(equacaoEmLista);
         }
     };
 
     public abstract Double executaEquacao(List<Object> equacaoEmLista);
+
+    public List<Object> fazMultiEDiviCientifica(Operacao operacao, List<Object> equacaoEmLista, int i) {
+        Double primeroNumero = (Double) equacaoEmLista.get(i - 1);
+        Double segundoNumero = (Double) equacaoEmLista.get(i + 1);
+        equacaoEmLista.set(i - 1, operacao.executarOperacao(primeroNumero, segundoNumero));
+        equacaoEmLista.remove(i);
+        equacaoEmLista.remove(i);
+        return equacaoEmLista;
+    }
+
 }
