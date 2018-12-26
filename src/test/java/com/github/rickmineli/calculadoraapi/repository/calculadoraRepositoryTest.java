0a924abd13bf878ef1bfa29720c4582c1741ae59
Ricardo Mineli <rick_mineli@hotmail.com>
2018-12-26 21:55:33 -0200

    Novos testes

--- src/test/java/com/github/rickmineli/calculadoraapi/repository/calculadoraRepositoryTest.java
+++ src/test/java/com/github/rickmineli/calculadoraapi/repository/calculadoraRepositoryTest.java
@@ -27,43 +27,65 @@ public class calculadoraRepositoryTest {
         calculoRepository.deleteAll();
     }
 
+    @Test
+    public void deveSalvarUmNovoCalculoSimples() {
+        List<Object> equacao = new ArrayList<>();
+        equacao.add(5.0);
+        equacao.add("SOMA");
+        equacao.add(7.0);
+        equacao.add("MULTIPLICACAO");
+        equacao.add(4.0);
+        equacao.add("SUBTRACAO");
+        equacao.add(10.0);
+        equacao.add("DIVISAO");
+        equacao.add(2.0);
+        Calculo novoCalculo = new Calculo(equacao, TipoDeCalculo.SIMPLES);
+        calculoRepository.save(novoCalculo);
+        List<Calculo> calculos = calculoRepository.findAll();
+        Assert.assertEquals(novoCalculo.getResultado(), calculos.get(0).getResultado());
+    }
 
     @Test
-    public void deveSalvarUmNovoCalculo(){
+    public void deveSalvarUmNovoCalculoCientifico() {
         List<Object> equacao = new ArrayList<>();
-        equacao.add(12.0);
+        equacao.add(5.0);
         equacao.add("SOMA");
-        equacao.add(2.0);
-        Calculo novoCalculo =  new Calculo(equacao, TipoDeCalculo.SIMPLES);
+        equacao.add(7.0);
+        equacao.add("MULTIPLICACAO");
+        equacao.add(4.0);
+        equacao.add("SUBTRACAO");
+        equacao.add(10.0);
+        equacao.add("DIVISAO");
+        equacao.add(2.0);    Calculo novoCalculo = new Calculo(equacao, TipoDeCalculo.CIENTIFICA);
         calculoRepository.save(novoCalculo);
         List<Calculo> calculos = calculoRepository.findAll();
         Assert.assertEquals(novoCalculo.getResultado(), calculos.get(0).getResultado());
     }
 
     @Test(expected = JSONInvalidoException.class)
-    public void naoDeveSalvarUmCalculoComEquacaoVazia(){
+    public void naoDeveSalvarUmCalculoComEquacaoVazia() {
         List<Object> equacao = new ArrayList<>();
-        Calculo novoCalculo =  new Calculo(equacao, TipoDeCalculo.SIMPLES);
+        Calculo novoCalculo = new Calculo(equacao, TipoDeCalculo.SIMPLES);
         calculoRepository.save(novoCalculo);
     }
 
     @Test(expected = JSONInvalidoException.class)
-    public void naoDeveSalvarUmCalculoComValoresNegativos(){
+    public void naoDeveSalvarUmCalculoComValoresNegativos() {
         List<Object> equacao = new ArrayList<>();
         equacao.add(-2.0);
         equacao.add("SUBTRACAO");
         equacao.add(-2.0);
-        Calculo novoCalculo =  new Calculo(equacao, TipoDeCalculo.SIMPLES);
+        Calculo novoCalculo = new Calculo(equacao, TipoDeCalculo.SIMPLES);
         calculoRepository.save(novoCalculo);
     }
 
     @Test(expected = JSONInvalidoException.class)
-    public void naoDeveSalvarUmCalculoComDivisaoPorZero(){
+    public void naoDeveSalvarUmCalculoComDivisaoPorZero() {
         List<Object> equacao = new ArrayList<>();
         equacao.add(2.0);
         equacao.add("DIVISAO");
         equacao.add(0.0);
-        Calculo novoCalculo =  new Calculo(equacao, TipoDeCalculo.SIMPLES);
+        Calculo novoCalculo = new Calculo(equacao, TipoDeCalculo.SIMPLES);
         calculoRepository.save(novoCalculo);
     }
 
