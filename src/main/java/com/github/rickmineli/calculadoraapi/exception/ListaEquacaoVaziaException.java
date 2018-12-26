b8d070a4e6bcf7fcb22eec7c54df4563fa961613
Ricardo Mineli <rick_mineli@hotmail.com>
2018-12-26 03:04:42 -0200

    Criado Exception para equacao vazia

---
+++ src/main/java/com/github/rickmineli/calculadoraapi/exception/ListaEquacaoVaziaException.java
@@ -0,0 +1,7 @@
+package com.github.rickmineli.calculadoraapi.exception;
+
+public class ListaEquacaoVaziaException extends RuntimeException {
+    public ListaEquacaoVaziaException(String message){
+        super(message);
+    }
+}
