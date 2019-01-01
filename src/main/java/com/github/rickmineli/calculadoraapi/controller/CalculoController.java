934cc9d8466ef8dc5b41b441fba7e47ae89271c7
Ricardo Mineli <rick_mineli@hotmail.com>
2019-01-01 16:14:03 -0200

    Adicionado GET todos os calculos

--- src/main/java/com/github/rickmineli/calculadoraapi/controller/CalculoController.java
+++ src/main/java/com/github/rickmineli/calculadoraapi/controller/CalculoController.java
@@ -4,11 +4,11 @@ import com.github.rickmineli.calculadoraapi.dto.CalculoDTO;
 import com.github.rickmineli.calculadoraapi.model.Calculo;
 import com.github.rickmineli.calculadoraapi.service.CalculoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin("http://localhost:8080")
 @RestController
 @RequestMapping("api/calculos")
 public class CalculoController {
@@ -19,5 +19,9 @@ public class CalculoController {
     @PostMapping
     public Calculo postCalculo(@RequestBody CalculoDTO calculo){
         return calculoService.salvaNovoCalculo(calculo.getEquacaoEmLista(),calculo.getTipoDeCalculo());
+    }
+    @GetMapping
+    public List<Calculo> todasCalculadoras(){
+        return calculoService.buscarTodosOsCalculos();
     }
 }
